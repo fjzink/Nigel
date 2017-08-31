353d3c27a2139f6f0977b00dcb93c24915501cfe
Fulton Zink <joeyzink@gmail.com>
2017-08-31 15:54:36 -0700

    Adds views for login.  Creates header partial for layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
 
   def student_login
+    @student = Student.new
   end
 
   def teacher_login
+    @teacher = Teacher.new
   end
 
   def create
@@ -11,13 +13,20 @@ class SessionsController < ApplicationController
     @student = Student.find_by(email: params[:session][:email].downcase)
     if @teacher && @teacher.authenticate(params[:session][:password])
       log_in_teacher(@teacher)
-      redirect_to teacher_path
+      redirect_to root_path
     else
-      @student && @student.authenticate(params[:session][:password])
+      flash[:error] = @teacher.errors.full_messages
+      render teacher_login_path
+    end
+
+    if @student && @student.authenticate(params[:session][:password])
       log_in_student(@student)
-      redirect_to student_path
+      redirect_to root_path
+    else
+      flash[:error] = @student.errors.full_messages
+      render student_login_path
+    end
   end
-end
 
   def destroy
     log_out
