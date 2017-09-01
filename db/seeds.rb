29bf3a0088572b0eb10a5890ed14f63a9a380c45
Fulton Zink <joeyzink@gmail.com>
2017-08-31 18:50:46 -0700

    Add show pages for students and teachers. Add url to resource migration.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ ResourceHashtag.delete_all
 
 Student.create(username: "filmon", email: "filmon@aol.com", password: "password")
 Teacher.create(username: "hunter", email: "hunter@barbie.com", password: "password")
-Resource.create(title: "Super cool title", content: "Angie is really #happy with this.")
+Resource.create(title: "Super cool title", content: "Angie is really #happy with this.", link: "http://www.google.com")
 Resource.create(title: "Super cool title number 2", content: "Angie #not really #happy with this.")
 Favorite.create(resource_id: Resource.first.id, favoritable_id: Student.first.id, favoritable_type: "Student")
 Favorite.create(resource_id: Resource.last.id, favoritable_id: Teacher.first.id, favoritable_type: "Teacher")
